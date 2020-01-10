4c7f5937bb631c122c2791094561b782c2c4772d
JD Gault <jgault@basecampcodingacademy.org>
2020-01-10 14:10:04 -0600

    resolved issue with signup/login. received logo from Oscar and will find a proper layout for the home screen.

--- caselotto/app/models.py
+++ caselotto/app/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Wallet(models.Model):
+    owner = models.OneToOneField('Profile', on_delete=models.CASCADE)
+    lotto_points = models.FloatField()
+
+class Case(models.Model):
+    owner = models.ForeignKey('Profile', on_delete=models.CASCADE)
+    opened = models.BooleanField(default=False)
+
+
+class Profile(models.Model):
+    profile_picture = models.URLField()
+    bio = models.TextField(max_length=500, blank=True)
+    location = models.CharField(max_length=30, blank=True)
+
+    def __str__(self):
+        return self.username
--- caselotto/app/views.py
+++ caselotto/app/views.py
@@ -5,11 +5,24 @@ from django.contrib.auth.decorators import login_required
 from django.urls import reverse_lazy
 from app import forms
 from django.views.generic import FormView
+from app.models import Wallet, Case, Profile
 
 # Create your views here.
 class Home(View):
     def get(self, request):
         return render(request, "home.html")
+
+class DailyCase(View):
+    def get(self, request):
+        return render(request, "daily-case.html")
+
+class CoinFlip(View):
+    def get(self, request):
+        return render(request, "coin-flip.html")
+
+class About(View):
+    def get(self, request):
+        return render(request, "about.html")
 
 class SignUpView(FormView):
     form_class = forms.SignUpForm
